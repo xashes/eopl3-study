15f590bff2aed42473980c36bf78fa62eac4991f
xashes <kellerx0319@gmail.com>
2019-10-07 17:58:30 +0800

    finish exercise 1.1

--- chapter01.rkt
+++ chapter01.rkt
@@ -39,3 +39,36 @@
   (check-true (in-2n+3m+1? 5))
   (check-true (in-2n+3m+1? 6))
   )
+
+;; 3. {(n, 2n + 1) | n ∈ N}
+(define/contract (in-pair-set? n m)
+  (-> natural? natural? boolean?)
+  (if (or (< n 0)
+          (< m 1))
+      #f
+      (or (and (zero? n) (= m 1))
+          (in-pair-set? (- n 1) (- m 2)))))
+(module+ test
+  (check-false (in-pair-set? 0 0))
+  (check-false (in-pair-set? 0 3))
+  (check-true (in-pair-set? 0 1))
+  (check-true (in-pair-set? 1 3))
+  (check-true (in-pair-set? 2 5))
+  )
+
+;; 4. {(n, n^2) | n ∈ N}
+(define/contract (in-n-nsqr? n m)
+  (-> natural? natural? boolean?)
+  (if (or (< n 0)
+          (< m 0))
+      #f
+      (or (and (zero? n)
+               (zero? m))
+          (in-n-nsqr? (sub1 n) (add1 (- m (* n 2)))))))
+(module+ test
+  (check-false (in-n-nsqr? 0 1))
+  (check-false (in-n-nsqr? 1 9))
+  (check-true (in-n-nsqr? 0 0))
+  (check-true (in-n-nsqr? 4 16))
+  (check-true (in-n-nsqr? 8 64))
+  )
