0398f943dd95b0cfb2464d087479391014561a2e
xashes <kellerx0319@gmail.com>
2020-03-26 16:48:22 +0800

    1.30

--- chapter01.py
+++ chapter01.py
@@ -4,7 +4,7 @@ def everyp(pred, lst):
 
 
 def test_everyp():
-    assert everyp(lambda x: isinstance(x, int), ['a', 1, 2, 3]) is False
+    assert everyp(lambda x: isinstance(x, int), ["a", 1, 2, 3]) is False
     assert everyp(lambda x: x > 6, [7, 8, 9]) is True
 
 
@@ -13,5 +13,50 @@ def existp(pred, lst):
 
 
 def test_existp():
-    assert existp(lambda x: isinstance(x, int), ['a', 1, 'b']) is True
-    assert existp(lambda x: isinstance(x, int), ['a', 'c', 'b']) is False
+    assert existp(lambda x: isinstance(x, int), ["a", 1, "b"]) is True
+    assert existp(lambda x: isinstance(x, int), ["a", "c", "b"]) is False
+
+
+# Exercise 1.28
+def merge(loi1, loi2):
+    if len(loi1) == 0:
+        return loi2
+    elif len(loi2) == 0:
+        return loi1
+    elif loi1[0] < loi2[0]:
+        return [loi1[0]] + (merge(loi1[1:], loi2))
+    else:
+        return [loi2[0]] + (merge(loi1, loi2[1:]))
+
+
+def test_merge():
+    assert merge([1, 4], [1, 2, 8]) == [1, 1, 2, 4, 8]
+    assert merge([35, 62, 81, 90, 91], [3, 83, 85, 90]) == [
+        3,
+        35,
+        62,
+        81,
+        83,
+        85,
+        90,
+        90,
+        91,
+    ]
+
+
+# Exercise 1.29
+def qsort(lon):
+    if len(lon) == 0:
+        return []
+    else:
+        base = lon[0]
+        return (
+            qsort([n for n in lon[1:] if n <= base])
+            + [base]
+            + qsort([n for n in lon[1:] if n > base])
+        )
+
+
+def test_qsort():
+    lon = [8, 2, 5, 2, 3]
+    assert qsort(lon) == sorted(lon)
--- chapter01.rkt
+++ chapter01.rkt
@@ -441,3 +441,89 @@
   (check-true (exists? number? '(a b c 3 e)))
   (check-false (exists? number? '(a b c d e)))
   )
+
+;; Exercise 1.26
+(define/contract (up lst)
+  (-> list? list?)
+  (if (null? lst)
+      null
+      (let ([c (car lst)]
+            [cd (cdr lst)])
+        (if (list? c)
+            (append c (up cd))
+            (cons c (up cd)))))
+  )
+(module+ test
+  (check-equal? (up '((1 2) (3 4)))
+                '(1 2 3 4))
+  (check-equal? (up '((x (y)) () z))
+                '(x (y) z))
+  )
+
+;; Exercise 1.27
+(define/contract (flatten lst)
+  (-> list? list?)
+  (if (null? lst)
+      null
+      (let ([c (car lst)]
+            [cd (cdr lst)])
+        (if (list? c)
+            (append (flatten c)
+                    (flatten cd))
+            (cons c (flatten cd)))))
+  )
+(module+ test
+  (check-equal? (flatten '((1 2) (3 4)))
+                '(1 2 3 4))
+  (check-equal? (flatten '((x (y)) () z))
+                '(x y z))
+  )
+
+;; Exercise 1.28
+(define/contract (merge loi1 loi2)
+  (-> (listof integer?) (listof integer?) (listof integer?))
+  (cond
+    [(null? loi1) loi2]
+    [(null? loi2) loi1]
+    [else (let ([c1 (car loi1)]
+                [c2 (car loi2)])
+            (if (< c1 c2)
+                (cons c1 (merge (cdr loi1) loi2))
+                (cons c2 (merge loi1 (cdr loi2)))))]
+    )
+  )
+(module+ test
+  (check-equal? (merge '(1 4) '(1 2 8))
+                '(1 1 2 4 8))
+  (check-equal? (merge '(35 62 81 90 91) '(3 83 85 90))
+                '(3 35 62 81 83 85 90 90 91))
+  )
+
+;; Exercise 1.29
+(define/contract (qsort lon)
+  (-> (listof number?) (listof number?))
+  (sort/predicate < lon)
+  )
+(module+ test
+  (let ([lon '(8 2 5 2 3)])
+    (check-equal? (qsort lon)
+                  (sort lon <)))
+  )
+
+;; Exercise 1.30
+(define/contract (sort/predicate pred lon)
+  (-> procedure? (listof number?) (listof number?))
+  (if (empty? lon)
+      null
+      (let ([base (car lon)])
+        (append (sort/predicate pred (filter (lambda (n) (pred n base)) (cdr lon)))
+                (list base)
+                (sort/predicate pred (filter (lambda (n) (not (pred n base))) (cdr lon))))))
+  )
+(module+ test
+  (let ([lon '(8 2 5 2 3)])
+    (check-equal? (sort/predicate < lon)
+                  (sort lon <))
+    (check-equal? (sort/predicate > lon)
+                  (sort lon >)))
+  )
